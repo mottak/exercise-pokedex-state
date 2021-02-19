cd05e96f6721e10ba432a25527d50a8e8c13f2b5
kissyla <kissylamottapereira@gmail.com>
2021-02-19 10:16:58 -0300

    adicionando classe aos elementos do card Pokemon

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import './Pokemon.css';
 
 class Pokemon extends React.Component {
     render() {
         const { name, type, averageWeight, image } = this.props.props;
 
         return (
-            <div className="pokemon">
-                <div>
-                    <p> {name} </p>
-                    <p> {type} </p>
-                    <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
-                </div>
-                <img src={image} alt={`${name} sprite`} />
-            </div>
+            <div className="Pokemon-card">
+            <img src={image} alt={`Nome do Pokemon: ${name}`} className="Pokemon-image" />
+            <p className="Pokemon-Name">
+              Name:
+              {' '}
+              {name}
+            </p>
+            <p className="Pokemon-Type">
+              Type:
+              {' '}
+              {type}
+            </p>
+            <p className="Pokemon-Weight">{`Weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+          </div>
         );
     }
 }
